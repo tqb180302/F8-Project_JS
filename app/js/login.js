30278c6d0e76c8dd4c64dd90387c2d2c9418a368
tqb180302 <tqb180302@gmail.com>
2026-03-29 01:06:29 +0700

    add login page with token storage

---
+++ app/js/login.js
@@ -0,0 +1,39 @@
+
+const formDangNhap = document.getElementById('login-form');
+
+formDangNhap.addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const emailDienVao = document.getElementById('email').value;
+    const matKhauDienVao = document.getElementById('password').value;
+
+    console.log("1. Đang gửi dữ liệu lên Server...");
+
+    try {
+        const response = await fetch('https://k305jhbh09.execute-api.ap-southeast-1.amazonaws.com/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: emailDienVao,
+                password: matKhauDienVao
+            })
+        });
+
+        const data = await response.json();
+
+
+        if (response.ok) {
+            console.log("2. Đăng nhập thành công! Token của bạn là:", data.token);
+            localStorage.setItem('accessToken', data.token);
+            window.location.href = 'index.html';
+        } else {
+
+            alert("Đăng nhập thất bại: " + data.message);
+            console.log("Lỗi từ server:", data);
+        }
+    } catch (error) {
+        console.log("Lỗi mạng không gọi được API:", error);
+    }
+});
